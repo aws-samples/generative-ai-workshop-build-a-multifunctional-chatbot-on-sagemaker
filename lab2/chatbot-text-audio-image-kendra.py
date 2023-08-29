172ad038ec4ee09babd36a5f67c66b7b6d981990
melanie <mmelli@amazon.com>
2023-08-29 07:10:38 +0000

    update TGI container version

--- lab2/chatbot-text-audio-image-kendra.py
+++ lab2/chatbot-text-audio-image-kendra.py
@@ -44,63 +44,6 @@ endpoint_names = {
 }
 
 
-################# Prepare for RAG solution #######################
-class SagemakerEndpointEmbeddingsJumpStart(SagemakerEndpointEmbeddings):
-    def embed_documents(self, texts: List[str], chunk_size: int = 5) -> List[List[float]]:
-        """Compute doc embeddings using a SageMaker Inference Endpoint.
-
-        Args:
-            texts: The list of texts to embed.
-            chunk_size: The chunk size defines how many input texts will
-                be grouped together as request. If None, will use the
-                chunk size specified by the class.
-
-        Returns:
-            List of embeddings, one for each text.
-        """
-        results = []
-        _chunk_size = len(texts) if chunk_size > len(texts) else chunk_size
-
-        for i in range(0, len(texts), _chunk_size):
-            response = self._embedding_func(texts[i : i + _chunk_size])
-            print
-            results.extend(response)
-        return results
-
-
-class ContentHandlerEmbed(EmbeddingsContentHandler):
-    content_type = "application/json"
-    accepts = "application/json"
-
-    def transform_input(self, prompt: str, model_kwargs={}) -> bytes:
-        input_str = json.dumps({"text_inputs": prompt, **model_kwargs})
-        return input_str.encode("utf-8")
-
-    def transform_output(self, output: bytes) -> str:
-        response_json = json.loads(output.read().decode("utf-8"))
-        embeddings = response_json["embedding"]
-        return embeddings
-
-
-content_handler_embed = ContentHandlerEmbed()
-
-embeddings = SagemakerEndpointEmbeddingsJumpStart(
-    endpoint_name=embedding_endpoint_name,
-    region_name=aws_region,
-    content_handler=content_handler_embed,
-)
-
-@st.cache_resource
-def generate_index():
-    loader = DirectoryLoader("./data/demo-video-sagemaker-doc/", glob="**/*.txt")
-    documents = loader.load()
-    docsearch = FAISS.from_documents(documents, embeddings)
-    return docsearch
-
-docsearch = generate_index()
-
-
-
 
 ################# Prepare for chatbot with memory #######################
 
@@ -297,14 +240,6 @@ with left_column:
             else:
                 st.session_state.option = "NLP"
                 if rag:                    
-                    # docs = docsearch.similarity_search_with_score(user_input)
-                    # contexts = []
-                    # for doc, score in docs:
-                    #     print(f"Content: {doc.page_content}, Metadata: {doc.metadata}, Score: {score}")
-                    #     if score <= 0.8:
-                    #         contexts.append(doc)
-                    #         source.append(doc.metadata['source'])
-                    # print(f"\n INPUT CONTEXT:{contexts}")
                     
                     prompt_template = """Use the following pieces of Context to answer the Question at the end. If you don't know the answer, just say you don't know, don't try to make up an answer. Context:\n\n{context}\n\nQuestion: {question}\nHelpful Answer:"""
                     PROMPT = PromptTemplate(
